bf6c426f0fe0aaf2d5f6ab1a7068f4d8bb161dee
Daniel Svensson <dsvensson@gmail.com>
2024-05-16 08:14:44 +0200

    MINOR: Include stdint.h for VS compat.

--- include/rng_gen_state.h
+++ include/rng_gen_state.h
@@ -1,6 +1,8 @@
 #ifndef __RNG_STATE_H__
 #define __RNG_STATE_H__
 
+#include <stdint.h>
+
 // You must initialize this with rng_seed/rng_seed_global!
 typedef struct RngState { uint32_t s[4]; } RngState;
 
